dd54509e36240eb5ed6663dfda2003b048183994
orlane111 <nguessamiensa137@gmail.com>
2025-05-03 15:57:05 +0000

    Initialiser le backend (Node.js + Express)

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// index.js ou server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); // Pour parser le corps des requêtes en JSON
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
